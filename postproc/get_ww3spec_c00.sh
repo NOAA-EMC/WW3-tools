c695f2906dd83ec773ea572b4329f6bd5f068427
ricampos <ricardo.campos@noaa.gov>
2021-11-05 21:14:31 -0400

    ftp uptade to gfswave in get_ww3spec_c00.sh

--- postproc/get_ww3spec_c00.sh
+++ postproc/get_ww3spec_c00.sh
@@ -8,7 +8,7 @@ source /etc/bash.bashrc
 . /home/ricardo/python/anaconda/setanaconda3.sh
 
 # server address
-SERVER="https://www.ftp.ncep.noaa.gov/data/nccf/com/wave/prod"
+SERVER="https://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod"
 # work directory
 DIR="/home/ricardo/teste"
 # Spectral point of interest, see https://polar.ncep.noaa.gov/waves/viewer.shtml?-gfswave-
@@ -22,16 +22,15 @@ HORA="00" # first cycle 00Z
 
 cd ${DIR}
 # Download NCEP/NOAA sprecta
-wget --no-check-certificate --no-proxy -l1 -H -t1 -nd -N -np -erobots=off --tries=3 "${SERVER}/multi_1."${ANO}${MES}${DIA}"/multi_1.t${HORA}z.spec_tar.gz" -O ${DIR}/ww3spec_${ANO}${MES}${DIA}${HORA}.tgz >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
-# 
-tar -zxvf ${DIR}/ww3spec_${ANO}${MES}${DIA}${HORA}.tgz >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
-
+wget --no-check-certificate --no-proxy -l1 -H -t1 -nd -N -np -erobots=off --tries=3 "${SERVER}/gfs."${ANO}${MES}${DIA}"/"${HORA}"/wave/station/gfswave.t00z.spec_tar.gz" -O "${DIR}/ww3spec_${ANO}${MES}${DIA}${HORA}.tgz" >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
+#
+tar -zxvf ${DIR}"/ww3spec_"${ANO}${MES}${DIA}${HORA}".tgz" >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
 # Run python to plot spec
-python3 ww3pointspec.py ${DIR}"/multi_1."${spt}".spec" ${spt} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
+python3 ww3pointspec.py "${DIR}/gfswave."${spt}".spec" ${spt} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
 
 # Final organizing
-mkdir ${DIR}/"ww3spec_"${ANO}${MES}${DIA}${HORA} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
-mv ${DIR}/multi_1.*.spec ${DIR}/"ww3spec_"${ANO}${MES}${DIA}${HORA} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
-mv ${DIR}/wspectrum_${spt}*.png ${DIR}/"ww3spec_"${ANO}${MES}${DIA}${HORA} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
+mkdir "${DIR}/ww3spec_"${ANO}${MES}${DIA}${HORA} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
+mv "${DIR}/gfswave.${spt}.spec" "${DIR}/ww3spec_"${ANO}${MES}${DIA}${HORA} >> ${DIR}/logWW3spec_$ANO$MES$DIA$HORA 2>&1
+mv "${DIR}/wspectrum_${spt}*.png" "${DIR}/ww3spec_"${ANO}${MES}${DIA}${HORA} >> ${DIR}/logWW3spec_${ANO}${MES}${DIA}${HORA} 2>&1
 
 
