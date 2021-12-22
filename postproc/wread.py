d5b656eac60299d2c11fe266bd4758d863d2d9db
ricampos <ricardo.campos@noaa.gov>
2021-12-21 19:11:17 -0500

    functions for data reading (ww3 and buoys) included in wread.py. And a multiple-subplot figure script for ww3 results comparison (comparevalww3board.py).

---
+++ postproc/wread.py
@@ -0,0 +1,317 @@
+# Read Wave data: WAVEWATCHIII results, and NDBC and Copernicus buoys.
+# tseriesnc = time series (table of integrated parameters versus time).
+# spec = wave spectrum
+
+import matplotlib
+# matplotlib.use('Agg')
+import time
+from time import strptime
+from calendar import timegm
+import xarray as xr
+import netCDF4 as nc
+import numpy as np
+from pylab import *
+import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+from matplotlib import ticker
+# import pickle
+import sys
+import warnings; warnings.filterwarnings("ignore")
+
+
+# TIME-SERIES
+
+# Observations NDBC, netcdf format
+def tseriesnc_ndbc(*args):
+	'''
+	Observations NDBC, time series/table, netcdf format
+	Input: file name (example: 46047h2016.nc)
+	Output: Values: time(datetime64),time(seconds since 1970),lat,lon; Arrays: sst,mslp,dwp,tmp,gst,wsp,wdir,hs,tm,tp,dm
+	'''
+	if len(args) == 1:
+		fname=np.str(args[0])
+	elif len(args) > 1:
+		sys.exit(' Too many inputs')
+
+	try:
+		ds = xr.open_dataset(fname); f=nc.Dataset(fname)
+	except:
+		sys.exit(" Cannot open "+fname)
+	else:
+		btm = f.variables['average_wpd'][:,0,0]; btp = f.variables['dominant_wpd'][:,0,0]
+		btime = np.array(f.variables['time'][:]).astype('double')
+		f.close(); del f
+		bdate = ds['time'].values[:]
+		blat = ds['latitude'].values[:]
+		blon = ds['longitude'].values[:]
+		bsst = ds['sea_surface_temperature'].values[:,0,0]
+		bmslp = ds['air_pressure'].values[:,0,0]
+		bdwp = ds['dewpt_temperature'].values[:,0,0]
+		btmp = ds['air_temperature'].values[:,0,0]
+		bgst = ds['gust'].values[:,0,0]	
+		bwsp = ds['wind_spd'].values[:,0,0]
+		bwdir = ds['wind_dir'].values[:]
+		bhs = ds['wave_height'].values[:,0,0]
+		# btm = ds['average_wpd'].values[:,0,0]
+		# btp = ds['dominant_wpd'].values[:,0,0]
+		bdm = ds['mean_wave_dir'].values[:,0,0]
+		ds.close(); del ds
+
+		# Automatic and basic Quality Control
+		bsst[np.abs(bsst)>70]=np.nan
+		bmslp[(bmslp<500)|(bmslp>1500)]=np.nan
+		bdwp[np.abs(bdwp)>80]=np.nan	
+		btmp[np.abs(btmp)>80]=np.nan
+		bgst[(bgst<0)|(bgst>200)]=np.nan
+		bwsp[(bwsp<0)|(bwsp>150)]=np.nan
+		bwdir[(bwdir<-180)|(bwdir>360)]=np.nan
+		bhs[(bhs<0)|(bhs>30)]=np.nan
+		btm[(btm<0)|(btm>40)]=np.nan
+		btp[(btp<0)|(btp>40)]=np.nan
+		bdm[(bdm<-180)|(bdm>360)]=np.nan
+
+		return bdate,btime,blat,blon,bsst,bmslp,bdwp,btmp,bgst,bwsp,bwdir,bhs,btm,btp,bdm
+
+# Observations Copernicus, netcdf format
+def tseriesnc_copernicus(*args):
+	'''
+	Observations NDBC, time series/table, netcdf format
+	Input: file name (example: 46047h2016.nc)
+	Output: Values: time(datetime64),time(seconds since 1970),lat,lon; Arrays: sst,mslp,dwp,tmp,gst,wsp,wdir,hs,tm,tp,dm
+	'''
+	if len(args) == 1:
+		fname=np.str(args[0])
+	elif len(args) > 1:
+		sys.exit(' Too many inputs')
+
+	try:
+		ds = xr.open_dataset(fname); f=nc.Dataset(fname)
+	except:
+		sys.exit(" Cannot open "+fname)
+	else:
+		btime = np.array(f.variables['TIME'][:]*24*3600 + timegm( strptime('195001010000', '%Y%m%d%H%M') )).astype('double')
+		f.close(); del f
+		bdate = ds['TIME'].values[:]
+		blat = np.nanmean(ds['LATITUDE'].values[:])
+		blon = np.nanmean(ds['LONGITUDE'].values[:])
+		bsst = np.nanmean(ds['TEMP'].values[:,:],axis=1) # SST 
+		bmslp = np.nanmean(ds['ATMS'].values[:,:],axis=1) # Pressure
+		bdwp = np.nanmean(ds['DEWT'].values[:,:],axis=1) # Dewpoint
+		btmp = np.nanmean(ds['DRYT'].values[:,:],axis=1) # temperature
+		bgst = np.nanmean(ds['GSPD'].values[:,:],axis=1) # gust
+		bwsp = np.nanmean(ds['WSPD'].values[:,:],axis=1) # wind speed		
+		bwdir = np.nanmean(ds['WDIR'].values[:,:],axis=1) # wind direction		
+		bhs = np.nanmean(ds['VHM0'].values[:,:],axis=1) # Hs
+		btm = np.nanmean(ds['VTM02'].values[:,:],axis=1) # Tm
+		btp = np.nanmean(ds['VTPK'].values[:,:],axis=1) # Tp
+		bdm = np.nanmean(ds['VMDR'].values[:,:],axis=1) # Mean direction
+
+		# Automatic and basic Quality Control
+		bsst[np.abs(bsst)>70]=np.nan
+		bmslp[(bmslp<500)|(bmslp>1500)]=np.nan
+		bdwp[np.abs(bdwp)>80]=np.nan	
+		btmp[np.abs(btmp)>80]=np.nan
+		bgst[(bgst<0)|(bgst>200)]=np.nan
+		bwsp[(bwsp<0)|(bwsp>150)]=np.nan
+		bwdir[(bwdir<-180)|(bwdir>360)]=np.nan
+		bhs[(bhs<0)|(bhs>30)]=np.nan
+		btm[(btm<0)|(btm>40)]=np.nan
+		btp[(btp<0)|(btp>40)]=np.nan
+		bdm[(bdm<-180)|(bdm>360)]=np.nan
+		ds.close(); del ds
+
+		return bdate,btime,blat,blon,bsst,bmslp,bdwp,btmp,bgst,bwsp,bwdir,bhs,btm,btp,bdm
+
+# WAVEWATCH III point output, netcdf format
+def tseriesnc_ww3(*args):
+	'''
+	WAVEWATCH III, time series/table, netcdf format
+	Input:  file name (example: ww3gefs.20160928_tab.nc), and station name (example: 41002)
+		  point output file created with ww3_ounf last lines options:
+		  T 1 
+		  2
+		  0
+		  T
+		  2
+	Output: Values: time(datetime64),time(seconds since 1970),lat,lon; Arrays: hs, tm, tp, dm, dp, spr, lm
+	'''
+	if len(args) == 2:
+		fname=np.str(args[0]); stname=np.str(args[1])
+	elif len(args) < 2 :
+		sys.exit(' Two inputs are required: file name and station name')
+	elif len(args) > 2:
+		sys.exit(' Too many inputs')
+
+	try:
+		ds = xr.open_dataset(fname); f=nc.Dataset(fname)
+	except:
+		sys.exit(" Cannot open "+fname)
+	else:
+		mtime = np.array(f.variables['time'][:]*24*3600 + timegm( strptime(np.str(f.variables['time'].units).split(' ')[2][0:4]+'01010000', '%Y%m%d%H%M') )).astype('double')
+		f.close(); del f
+	
+		auxstationname=ds['station_name'].values[:,:]; stationname=[]
+		for i in range(0,auxstationname.shape[0]):
+			stationname=np.append(stationname,"".join(np.array(auxstationname[i,:]).astype('str')))
+
+		inds=np.where(stationname[:]==stname)
+		if size(inds)>0:
+			inds=np.int(inds[0][0]); stname=np.str(stationname[inds])
+		else:
+			sys.exit(' Station '+stname+' not included in the ww3 output file, or wrong station ID')
+
+		mdate = ds['time'].values[:]
+		mlat = np.nanmean(ds['latitude'].values[:,inds])
+		mlon = np.nanmean(ds['longitude'].values[:,inds])
+		mhs = ds['hs'].values[:,inds]
+		mtp = np.zeros(mhs.shape[0],'f')*np.nan
+		mtp[ds['fp'].values[:,inds]>0.0] = 1./ds['fp'].values[:,inds][ds['fp'].values[:,inds]>0.0]
+		mtm = ds['tr'].values[:,inds]
+		mdp = ds['th1p'].values[:,inds]
+		mdm = ds['th1m'].values[:,inds]
+		spr = ds['sth1m'].values[:,inds]
+		lm = ds['lm'].values[:,inds]
+		ds.close(); del ds
+
+	return mdate,mtime,mlat,mlon,mhs,mtm,mtp,mdm,mdp,spr,lm
+
+# SPECTRA 
+
+# Observations NDBC, netcdf format
+def spec_ndbc(*args):
+	'''
+	Observations NDBC, wave spectrum, netcdf format
+	Input: file name (example: 46047w2016.nc)
+	Output: Values: time(datetime64),time(seconds since 1970),lat,lon; Arrays: freq,dfreq,pspec,dmspec,dpspec,r1spec,r2spec
+	'''
+	if len(args) == 1:
+		fname=np.str(args[0])
+	elif len(args) > 1:
+		sys.exit(' Too many inputs')
+
+	try:
+		ds = xr.open_dataset(fname); f=nc.Dataset(fname)
+	except:
+		sys.exit(" Cannot open "+fname)
+	else:
+		btime = np.array(f.variables['time'][:]).astype('double')
+		f.close(); del f
+		bdate = ds['time'].values[:]
+		blat = ds['latitude'].values[:]
+		blon = ds['longitude'].values[:]
+		freq = ds['frequency'].values[:]
+		pspec = ds['spectral_wave_density'].values[:,:,0,0]
+		dmspec = ds['mean_wave_dir'][:,:,0,0]
+		dpspec = ds['principal_wave_dir'][:,:,0,0]	
+		r1spec = ds['wave_spectrum_r1'][:,:,0,0]
+		r2spec = ds['wave_spectrum_r2'][:,:,0,0]
+		ds.close(); del ds
+		# # DF in frequency (dfreq), https://www.ndbc.noaa.gov/wavespectra.shtml
+		dfreq=np.zeros(47,'f')
+		dfreq[0]=0.010; dfreq[1:14]=0.005; dfreq[14:40]=0.010; dfreq[40::]=0.020
+		pspec=np.array(pspec*dfreq)
+
+	return bdate,btime,blat,blon,freq,dfreq,pspec,dmspec,dpspec,r1spec,r2spec
+
+
+# WAVEWATCH III spectra output, netcdf format
+def spec_ww3(*args):
+	'''
+	WAVEWATCH III, wave spectrum, netcdf format
+	Input: file name (example: ww3gefs.20160928_spec.nc), and station name (example: 41002)
+	Output: Values: time(datetime64),time(seconds since 1970),lat,lon; Arrays: freq,dfreq,pwst,d1sp,dire,dspec
+	'''
+	if len(args) == 2:
+		fname=np.str(args[0]); stname=np.str(args[1])
+	elif len(args) < 2 :
+		sys.exit(' Two inputs are required: file name and station name')
+	elif len(args) > 2:
+		sys.exit(' Too many inputs')
+
+	try:
+		ds = xr.open_dataset(fname); f=nc.Dataset(fname)
+	except:
+		sys.exit(" Cannot open "+fname)
+	else:
+
+		mtime = np.array(f.variables['time'][:]*24*3600 + timegm( strptime(np.str(f.variables['time'].units).split(' ')[2][0:4]+'01010000', '%Y%m%d%H%M') )).astype('double')
+		f.close(); del f
+
+		# Spectrum
+		dspec=np.array(ds['efth'].values[:,:,:,:])
+		# number of directions
+		nd=dspec.shape[3]
+		# number of frequencies
+		nf=dspec.shape[2]
+		# directions
+		dire=np.array(ds['direction'].values[:])
+		# frequencies
+		freq=np.array(ds['frequency'].values[:])
+		freq1=np.array(ds['frequency1'].values[:])
+		freq2=np.array(ds['frequency2'].values[:])
+		# DF in frequency (dfreq)
+		dfreq=np.array(freq2 - freq1)
+		# wind intensity and wind direction
+		wnds=np.array(ds['wnd'].values[:,:])
+		wndd=np.array(ds['wnddir'].values[:,:])
+		# Time datetime64 array
+		mdate=np.array(ds['time'].values[:])
+		# water depth (constant in time)
+		depth=np.nanmean(ds['dpt'].values[:,:],axis=0)
+		auxstationname=ds['station_name'].values[:,:]; stationname=[]
+		for i in range(0,auxstationname.shape[0]):
+			stationname=np.append(stationname,"".join(np.array(auxstationname[i,:]).astype('str')))
+
+		lon=np.array(np.nanmean(ds['longitude'].values[:,:],axis=0))
+		lat=np.array(np.nanmean(ds['latitude'].values[:,:],axis=0))	
+		ds.close(); del ds, auxstationname
+
+		inds=np.where(stationname[:]==stname)
+		if size(inds)>0:
+			inds=np.int(inds[0][0]); stname=np.str(stationname[inds])
+		else:
+			sys.exit(' Station '+stname+' not included in the output file, or wrong station ID')
+
+		dspec=np.array(dspec[:,inds,:,:]); wnds=np.array(wnds[:,inds]); wndd=np.array(wndd[:,inds])
+		lat=np.array(lat[inds]); lon=np.array(lon[inds]); depth=np.array(depth[inds])
+		del inds, stationname
+		# ------------------
+		# 1D power spectrum
+		pwst=np.zeros((dspec.shape[0],nf),'f')
+		for t in range(0,dspec.shape[0]):
+			for il in range(0,nf):
+				pwst[t,il]=sum(dspec[t,il,:]*(2*np.pi)/nd)
+
+			pwst[t,:]=pwst[t,:]*dfreq[:]
+
+		# organizing directions  -----
+		adspec=np.copy(dspec); inddire=int(np.where(dire==min(dire))[0][0])
+		for t in range(0,dspec.shape[0]):
+			adspec[t,:,0:nd-(inddire+1)]=dspec[t,:,(inddire+1):nd]
+			adspec[t,:,nd-(inddire+1):nd]=dspec[t,:,0:(inddire+1)]
+			for i in range(0,nd):
+				dspec[t,:,i]=adspec[t,:,nd-i-1]
+
+			adspec[t,:,0:int(nd/2)]=dspec[t,:,int(nd/2):nd]
+			adspec[t,:,int(nd/2):nd]=dspec[t,:,0:int(nd/2)]
+			dspec[t,:,:]=adspec[t,:,:]
+
+		dire=np.sort(dire)
+
+		# 1D directional spectrum
+		d1sp=np.zeros((dspec.shape[0],nf),'f')
+		for t in range(0,dspec.shape[0]):
+			for il in range(0,nf):	
+				a = np.sum(dspec[t,il,:] * np.array(np.sin((pi*dire)/180.)/np.sum(dspec[t,il,:])) )
+				b = np.sum(dspec[t,il,:] * np.array(np.cos((pi*dire)/180.)/np.sum(dspec[t,il,:])) )
+				aux = math.atan2(a,b)*(180./pi)
+				if aux<0:
+					aux=aux+360.
+				
+				d1sp[t,il]=np.float(aux)
+				del a,b,aux
+
+	return mdate,mtime,lat,lon,freq,freq1,freq2,dfreq,pwst,d1sp,dire,dspec
+
+
