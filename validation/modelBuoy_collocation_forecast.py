7370aa0b9d03d3971195985bb6bf3ea440f0004e
ricampos <ricardo.campos@noaa.gov>
2022-02-28 19:09:23 -0500

    add better time array structure for forecast files, ww3 buoy collocation

--- validation/modelBuoy_collocation_forecast.py
+++ validation/modelBuoy_collocation_forecast.py
@@ -1,11 +1,12 @@
-# Collocation/pairing ww3 point output results with wave buoys.
-# This code is restricted to ww3 hindcasts, where multiple files can be appended, creating a continuous time array.
+# Collocation/pairing ww3 point output forecast results with wave buoys.
+# This code is designed for ww3 forecasts, with two time dimensions (cycle time, and forecast lead time)
 
 import numpy as np
 from matplotlib.mlab import *
 from pylab import *
 import xarray as xr
 import netCDF4 as nc
+import numpy as np
 import time
 from time import strptime
 from calendar import timegm
@@ -16,61 +17,65 @@ fnetcdf="NETCDF4"
 # Users have to check the buoys' names at the "f=nc.Dataset" lines below.
 
 # Paths
-# WW3 Model
-mpath="/ww3runs/c00"
-# NDBC buoys
-ndbcp="/data/buoys/NDBC/wparam"
-# Copernicus buoys
-copernp="/data/buoys/Copernicus/wtimeseries"
-# read list of ww3 files to be included in the collocation
+mpath="/work/noaa/marine/ricardo.campos/work/ww3runs/results/stream01/c00"
+ndbcp="/work/noaa/marine/ricardo.campos/data/buoys/NDBC/ncformat/wparam"
+copernp="/work/noaa/marine/ricardo.campos/data/buoys/Copernicus/wtimeseries"
 # import os; os.system("ls "+mpath+"/*tab.nc > ww3list.txt &")
 wlist=np.loadtxt('ww3list.txt',dtype=str)
 
 # Read Data
 # Model
 for t in range(0,size(wlist)):
+
 	try:
 		ds = xr.open_dataset(wlist[t]); f=nc.Dataset(wlist[t])
 	except:
 		print(" Cannot open "+wlist[t])
 	else:
-		# list of station/buoy names
 		if t==0:
+			# forecast lead time
+			flt = np.array(f.variables['time'][:]*24*3600 + timegm( strptime(np.str(f.variables['time'].units).split(' ')[2][0:4]+'01010000', '%Y%m%d%H%M') )).astype('double')
+			mtime=np.array([flt]).astype('double')
+			sflt=np.array( flt - np.nanmin(flt) ).astype('double')
+			# list of station/buoy names
 			auxstationname=ds['station_name'].values[:,:]; stname=[]
 			for i in range(0,auxstationname.shape[0]):
 				stname=np.append(stname,"".join(np.array(auxstationname[i,:]).astype('str')))
 
+			# Initial arrays
+			# stream/test, members, point, cycle/time, forecast_lead_time
+			mhs=np.zeros((size(stname),size(wlist),flt.shape[0]),'f')*np.nan
+			mtm=np.zeros((size(stname),size(wlist),flt.shape[0]),'f')*np.nan
+			mdm=np.zeros((size(stname),size(wlist),flt.shape[0]),'f')*np.nan
+			# buoy data
+			bhs=np.zeros((size(stname),size(wlist),flt.shape[0]),'f')*np.nan
+			btm=np.zeros((size(stname),size(wlist),flt.shape[0]),'f')*np.nan
+			bdm=np.zeros((size(stname),size(wlist),flt.shape[0]),'f')*np.nan
+			lat=np.zeros(size(stname),'f')*np.nan; lon=np.zeros(size(stname),'f')*np.nan
+
+		else:
+			flt = np.array(f.variables['time'][:]*24*3600 + timegm( strptime(np.str(f.variables['time'].units).split(' ')[2][0:4]+'01010000', '%Y%m%d%H%M') )).astype('double')
+			mtime = np.append(mtime,np.array([flt[0]+sflt]).astype('double'),axis=0)
+
 		ahs = np.array(ds['hs'].values[:,:]).T
 		atm = np.array(ds['tr'].values[:,:]).T
 		adm = np.array(ds['th1m'].values[:,:]).T
-		at = np.array(f.variables['time'][:]*24*3600 + timegm( strptime(np.str(f.variables['time'].units).split(' ')[2][0:4]+'01010000', '%Y%m%d%H%M') )).astype('double')
-		ds.close();f.close(); del ds,f
-		if t==0:
-			mhs=np.copy(ahs)
-			mtm=np.copy(atm)
-			mdm=np.copy(adm)
-			mtime=np.copy(at)
-		else:
-			mhs=np.append(mhs,ahs,axis=1)
-			mtm=np.append(mtm,atm,axis=1)
-			mdm=np.append(mdm,adm,axis=1)
-			mtime=np.append(mtime,at)
+		ds.close(); del ds
+		ignore,ind,ignore = np.intersect1d(mtime[t,:], flt, return_indices=True)
+		mhs[:,t,ind] = ahs
+		mtm[:,t,ind] = atm
+		mdm[:,t,ind] = adm
+		del ahs,atm,adm
 
-		del ahs,atm,adm,at
+	print("done "+repr(t))
 
-# Buoys
-bhs=np.zeros((size(stname),size(mtime)),'f')*np.nan
-btm=np.zeros((size(stname),size(mtime)),'f')*np.nan
-bdm=np.zeros((size(stname),size(mtime)),'f')*np.nan
-lat=np.zeros(size(stname),'f')*np.nan; lon=np.zeros(size(stname),'f')*np.nan
 # help reading NDBC buoys, divided by year
 yrange=np.array(np.arange(time.gmtime(mtime.min())[0],time.gmtime(mtime.min())[0]+1,1)).astype('int')
-# loop buoys
+# Buoys
 for b in range(0,size(stname)):
 
 	ahs=[]
 	try:
-
 		ahs=[];atm=[];adm=[];atime=[]
 		for y in yrange:
 
@@ -89,6 +94,7 @@ for b in range(0,size(stname)):
 			atm = np.nanmean(f.variables['VTM02'][:,:],axis=1)
 			adm = np.nanmean(f.variables['VMDR'][:,:],axis=1)
 			atime = np.array(f.variables['TIME'][:]*24*3600 + timegm( strptime('195001010000', '%Y%m%d%H%M') )).astype('double')
+			# lat[b] = f.variables['LATITUDE'][:]; lon[b] = f.variables['LONGITUDE'][:]
 			lat[b] = np.nanmean(f.variables['LATITUDE'][:]); lon[b] = np.nanmean(f.variables['LONGITUDE'][:])
 			f.close(); del f
 		except:
@@ -97,20 +103,19 @@ for b in range(0,size(stname)):
 	else:
 		if size(ahs>0):
 			c=0
-			for t in range(0,size(mtime)):
-				indt=np.where(np.abs(atime-mtime[t])<1800.)
-				if size(indt)>0:
-					if np.any(ahs[indt[0]].mask==False):
-						bhs[b,t] = np.nanmean(ahs[indt[0]][ahs[indt[0]].mask==False])
-						c=c+1
-					if np.any(atm[indt[0]].mask==False):
-						btm[b,t] = np.nanmean(atm[indt[0]][atm[indt[0]].mask==False])
-					if np.any(adm[indt[0]].mask==False):
-						bdm[b,t] = np.nanmean(adm[indt[0]][adm[indt[0]].mask==False])
+			for t in range(0,size(wlist)):
+				for i in range(0,mtime.shape[1]):
+					indt=np.where(np.abs(atime-mtime[t,i])<1800.)
+					if size(indt)>0:
+						if np.any(ahs[indt[0]].mask==False):
+							bhs[b,t,i] = np.nanmean(ahs[indt[0]][ahs[indt[0]].mask==False])
+							c=c+1
+						if np.any(atm[indt[0]].mask==False):
+							btm[b,t,i] = np.nanmean(atm[indt[0]][atm[indt[0]].mask==False])
+						if np.any(adm[indt[0]].mask==False):
+							bdm[b,t,i] = np.nanmean(adm[indt[0]][adm[indt[0]].mask==False])
 
-					del indt
-
-			# print("counted "+repr(c)+" at "+stname[b])
+			print("counted "+repr(c)+" at "+stname[b])
 
 	print("done "+stname[b])
 	del ahs
@@ -144,23 +149,25 @@ if size(ind)>0:
 lon[lon>180.]=lon[lon>180.]-360.
 inidate=np.str(time.gmtime(mtime.min())[0])+np.str(time.gmtime(mtime.min())[1]).zfill(2)+np.str(time.gmtime(mtime.min())[2]).zfill(2)+np.str(time.gmtime(mtime.min())[3]).zfill(2)
 findate=np.str(time.gmtime(mtime.max())[0])+np.str(time.gmtime(mtime.max())[1]).zfill(2)+np.str(time.gmtime(mtime.max())[2]).zfill(2)+np.str(time.gmtime(mtime.max())[3]).zfill(2)
+# Save netcdf
 ncfile = nc.Dataset("ww3buoy_collocation_"+inidate+"to"+findate+".nc", "w", format=fnetcdf) 
-ncfile.history="Collocation of WW3 point output (table) and NDBC/Copernicus Buoys. Total of "+repr(bhs[bhs>0.].shape[0])+" observations or pairs model/observation."
+ncfile.history="Collocation of WW3 point output forecast (table) and NDBC/Copernicus Buoys. Total of "+repr(bhs[bhs>0.].shape[0])+" observations or pairs model/observation."
 # create  dimensions. 2 Dimensions
 ncfile.createDimension('buoypoints', bhs.shape[0] )
-ncfile.createDimension('time', bhs.shape[1] )
+ncfile.createDimension('fcycletime', bhs.shape[1] )
+ncfile.createDimension('forecastleadtime', bhs.shape[2] )
 # create variables.
-vt = ncfile.createVariable('time',np.dtype('float64').char,('time'))
+vt = ncfile.createVariable('time',np.dtype('float64').char,('fcycletime','forecastleadtime'))
 vstname = ncfile.createVariable('buoyID',dtype('a25'),('buoypoints'))
 vlat = ncfile.createVariable('latitude',np.dtype('float32').char,('buoypoints'))
 vlon = ncfile.createVariable('longitude',np.dtype('float32').char,('buoypoints'))
 #
-vmhs = ncfile.createVariable('model_hs',np.dtype('float32').char,('buoypoints','time'))
-vmtm = ncfile.createVariable('model_tm',np.dtype('float32').char,('buoypoints','time'))
-vmdm = ncfile.createVariable('model_dm',np.dtype('float32').char,('buoypoints','time'))
-vbhs = ncfile.createVariable('buoy_hs',np.dtype('float32').char,('buoypoints','time'))
-vbtm = ncfile.createVariable('buoy_tm',np.dtype('float32').char,('buoypoints','time'))
-vbdm = ncfile.createVariable('buoy_dm',np.dtype('float32').char,('buoypoints','time'))
+vmhs = ncfile.createVariable('model_hs',np.dtype('float32').char,('buoypoints','fcycletime','forecastleadtime'))
+vmtm = ncfile.createVariable('model_tm',np.dtype('float32').char,('buoypoints','fcycletime','forecastleadtime'))
+vmdm = ncfile.createVariable('model_dm',np.dtype('float32').char,('buoypoints','fcycletime','forecastleadtime'))
+vbhs = ncfile.createVariable('buoy_hs',np.dtype('float32').char,('buoypoints','fcycletime','forecastleadtime'))
+vbtm = ncfile.createVariable('buoy_tm',np.dtype('float32').char,('buoypoints','fcycletime','forecastleadtime'))
+vbdm = ncfile.createVariable('buoy_dm',np.dtype('float32').char,('buoypoints','fcycletime','forecastleadtime'))
 # Assign units
 vlat.units = 'degrees_north' ; vlon.units = 'degrees_east'
 vt.units = 'seconds since 1970-01-01T00:00:00+00:00'
@@ -168,14 +175,14 @@ vmhs.units='m'; vbhs.units='m'
 vmtm.units='s'; vbtm.units='s'
 vmdm.units='degrees'; vbdm.units='degrees'
 # Allocate Data
-vt[:]=mtime[:]; vstname[:]=stname[:]
+vt[:,:]=mtime[:,:]; vstname[:]=stname[:]
 vlat[:] = lat[:]; vlon[:] = lon[:]
-vmhs[:,:]=mhs[:,:]
-vmtm[:,:]=mtm[:,:]
-vmdm[:,:]=mdm[:,:]
-vbhs[:,:]=bhs[:,:]
-vbtm[:,:]=btm[:,:]
-vbdm[:,:]=bdm[:,:]
+vmhs[:,:,:]=mhs[:,:,:]
+vmtm[:,:,:]=mtm[:,:,:]
+vmdm[:,:,:]=mdm[:,:,:]
+vbhs[:,:,:]=bhs[:,:,:]
+vbtm[:,:,:]=btm[:,:,:]
+vbdm[:,:,:]=bdm[:,:,:]
 #
 ncfile.close()
 print(' ')
