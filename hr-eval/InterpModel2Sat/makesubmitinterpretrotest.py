aea701318b004c45a77d53ce3b79326c17a9ba54
Ming Chen <ming.chen1@noaa.gov>
2026-01-28 04:27:12 +0000

    Add machine definition support for interpolation

--- hr-eval/InterpModel2Sat/makesubmitinterpretrotest.py
+++ hr-eval/InterpModel2Sat/makesubmitinterpretrotest.py
@@ -5,6 +5,8 @@ import re
 import glob
 
 ## ===================== Setting (modified as needed) =========================
+MACHINE = "ursa" # machine name ursa/orion/hercules
+
 # directory settings
 rootdir = os.path.join('/scratch4/NCEPDEV/marine/Ming.Chen/wave_eval/processsatdata', 'jobinterp')
 MODEL_BASE = "/scratch3/NCEPDEV/climate/Jessica.Meixner/Data/retrov17_01"
@@ -13,17 +15,13 @@ OUTDIR_BASE = "/scratch4/NCEPDEV/marine/Ming.Chen/wave_eval/processsatdata/outin
 
 # satellite and model settings
 satellites=['JASON3', 'CRYOSAT2', 'SARAL', 'SENTINEL3A', 'SENTINEL3B', 'SENTINEL6A']
-model='retrov17_01'
+model='retrov17_01' # now only support model of GFSv16 and retrov17_01
 tz_list = ["00","06","12","18"]
 grid = "global.0p25"
 MODEL_DATA_PATTERN_TEMPLATE = "gfs.t{tz}z.{grid}.f*.grib2"
 
 # process script
 PROC_SCRIPT = "/scratch4/NCEPDEV/marine/Ming.Chen/wave_eval/WW3-tools/ww3tools/ProcSat_interpolation.py"
-
-# python and other modules used
-MODULE_USE_PATH = "/scratch4/NCEPDEV/marine/Saeideh.Banihashemi/installs/python-modules/"
-MODULE_LOAD     = "Ursa_ENV"
 
 # Slurm settings
 SBATCH_ACCOUNT   = "marine-cpu"
@@ -34,6 +32,19 @@ SBATCH_NTASKS        = 1
 SBATCH_CPUS_PER_TASK = 4
 SBATCH_MEM           = "16G"
 SET_THREAD_ENVS = True
+
+## --------------- Machine-specific configuration ------------------
+
+if MACHINE == "ursa":
+    MODULE_USE_PATH = "/scratch4/NCEPDEV/marine/Saeideh.Banihashemi/installs/python-modules/"
+    MODULE_LOAD     = "Ursa_ENV"
+elif MACHINE in ("orion", "hercules"):
+    MODULE_USE_PATH = "/work2/noaa/marine/jmeixner/general/modulefiles"
+    MODULE_LOAD     = "ww3tools"
+else:
+    print(f"ERROR: Unsupported MACHINE='{MACHINE}'. Use Ursa, Orion, or Hercules.", file=sys.stderr)
+    sys.exit(1)
+
 ## --------------- Checking inputs and settings --------------------
 
 jobdir = os.path.join(rootdir, model)
@@ -117,8 +128,14 @@ for cdate in cdates:
         continue
 
     for tz in tz_list:
-        model_gridded_dir = os.path.join(MODEL_BASE, f"gfs.{cdate}", tz, "products", "wave", "gridded","global.0p25")
-        #model_gridded_dir = os.path.join(MODEL_BASE, f"gfs.{cdate}", tz, "wave", "gridded")
+        if model == "GFSv16":
+            model_gridded_dir = os.path.join(MODEL_BASE, f"gfs.{cdate}", tz, "wave", "gridded")
+        elif model == "retrov17_01":
+            model_gridded_dir = os.path.join(MODEL_BASE, f"gfs.{cdate}", tz, "products", "wave", "gridded","global.0p25")
+        else:
+            print(f"ERROR: Unsupported Model.", file=sys.stderr)
+            sys.exit(1)
+
         if not os.path.isdir(model_gridded_dir):
             skipped_no_model_gribs += 1
             missing_cycles.append(f"{cdate}{tz}")
@@ -165,8 +182,14 @@ for cdate in cdates:
             f.write(f"OUTDIR={OUTDIR_BASE}\n")
             f.write("mkdir -p ${OUTDIR}\n\n")
 
-            f.write(f"MODEL_DATA_DIR={MODEL_BASE}/gfs.${{DATE}}/${{TZ}}/products/wave/gridded/global.0p25\n")
-            #f.write(f"MODEL_DATA_DIR={MODEL_BASE}/gfs.${{DATE}}/${{TZ}}/wave/gridded\n")
+            if model == "GFSv16":
+                f.write(f"MODEL_DATA_DIR={MODEL_BASE}/gfs.${{DATE}}/${{TZ}}/wave/gridded\n")
+            elif model == "retrov17_01":
+                f.write(f"MODEL_DATA_DIR={MODEL_BASE}/gfs.${{DATE}}/${{TZ}}/products/wave/gridded/global.0p25\n")
+            else:
+                print(f"ERROR: Unsupported Model.", file=sys.stderr)
+                sys.exit(1)
+
             f.write(f"MODEL_DATA_PATTERN='{pattern}'\n\n")
 
             for sat in satellites:
