c7de8ed532afbcfe4f2db66ed7a4b0b2f8fbd062
kestonsmith-noaa <keston.smith@noaa.gov>
2026-02-04 14:51:09 +0000

    Added python scripts and postprocessing for generating mesh with modified New Orleans Coast

---
+++ unst_msh_gen/RWPSMeshtoolkit/MeshGenTemplateDirectory/PostProcessGrid.OSMxGSHHS.NewOrl.m
@@ -0,0 +1,382 @@
+
+addpath ../matlab
+addpath /scratch3/NCEPDEV/climate/Keston.Smith/MeshGenMatlabLibs/jigsaw-matlab
+addpath /scratch3/NCEPDEV/climate/Keston.Smith/MeshGenMatlabLibs/InsidePoly
+
+%script to handle all post jigsaw mesh editing.
+%Post processing steps are as follows
+% (A) Remove sand points
+% (B) Merge inland lakes --> 'RWPS.WW3a.lakes.msh'
+% (C) Remove islands that jigsaw has meshed over 
+% (D) Remove sand points that can be created (rarely) with island removal --> 'RWPS.WW3a1.lakes.msh'
+% INACTIVE (E) Examine Pacific island resolution and make changes to island editing if needed --> 'RWPS.WW3b.lakes.msh'
+% INACTIVE (F) Examine mesh near New Orleans where boundary has been merged to reflect new marine zones --> 'RWPS.WW3c.lakes.msh'
+% INACTIVE (G) Remove sand points that can be created (rarely) with New Orleans editing --> 'RWPS.WW3d.lakes.msh'
+% (H) Stretch open ocean boundary nodes back to origonal specified boundary rectangle --> 'RWPS.WW3e.lakes.msh'
+%       this is an artificat of the projection used in jigsaw
+% (I) Remove sand points that can be created (Should not matter) with boundary node stretching --> 'RWPS.WW3f.lakes.msh'
+% (J) Write final WW3 mesh --> 'RWPS.WW3g.lakes.msh'
+%       this is the mesh to run WW3 on.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Input file from jigsaw and boundary file used in it's creation:
+isplot=0;
+outdir='RWPS.OSMxGSHHS.NewOrl/'
+pslgfile='/scratch3/NCEPDEV/climate/Keston.Smith/RWPS/Data/JigsawFormatFiles/PSLGboundaryOSMxGSHHS1kmBOXES.msh'
+jigsawout='RWPS.F.LLH'
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (A) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+g=loadmsh([outdir,jigsawout,'.msh']);
+
+%remove sand points on boundary
+
+RemoveSandPoints([outdir,jigsawout,'.msh'],pslgfile,[outdir,jigsawout,'.NSP.msh'],[outdir,jigsawout,'.NSP.WW3.msh']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (B) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Add Lakes to Mesh
+%g=loadmshWW3('RWPSMeshOSMxGSHHS.BoxesFiles/RWPS.F.LLH.NSP.WW3.msh');
+g=loadmshWW3( [outdir,jigsawout,'.NSP.WW3.msh']);
+
+gS=loadmshWW3('/scratch3/NCEPDEV/climate/Keston.Smith/RWPS/RWPSLakes/Sebago.NWPS.WW3.msh');
+gS.x=gS.x-360;
+gW=loadmshWW3('/scratch3/NCEPDEV/climate/Keston.Smith/RWPS/RWPSLakes/Winnipesaukee.NWPS.WW3.msh')
+gW.x=gW.x-360;
+gO=loadmshWW3('/scratch3/NCEPDEV/climate/Keston.Smith/RWPS/RWPSLakes/Okeechobee.NWPS.WW3.msh')
+gO.x=gO.x-360;
+
+g=CombineMesh(g,gO);
+g=CombineMesh(g,gS);
+g=CombineMesh(g,gW);
+
+x=g.x;y=g.y;z=g.z;e=g.e;
+
+if isplot,
+    LS=ComputeLengthScale_wgs84_MEL(x,y,e);LSn=Ele2Nodes(x,y,e,LS);
+    clf;ph=patch(x(e'),y(e'),LSn(e'));cm=colormap('jet');shading interp;axis equal;
+    caxis([0,12])
+    colormap(flip(cm));
+end
+
+WriteWW3MeshX(g,'RWPS.WW3a.lakes.msh');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (C) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%clear
+g=loadmshWW3('RWPS.WW3a.lakes.msh')
+%Remove key islands remaining in mesh that are present in the PSLG file
+
+p=loadmsh(pslgfile);
+
+p.x=p.point.coord(:,1);
+p.y=p.point.coord(:,2);
+p.edges=p.edge2.index(:,1:2);
+p.x=p.x-360;
+x=g.x;y=g.y;z=g.z;e=g.e;
+
+if isplot,
+    clf;ph=patch(x(e'),y(e'),z(e'));cm=colormap('jet');shading interp;axis equal;
+    hold on
+    plot(p.x,p.y,'k.')
+end
+
+MinBndDist=1000;
+gnew=g;
+
+%go through all longitude and remove nodes 
+dx=2;
+dxb=10
+%close all
+RemoveIsl=1;
+n=1
+if RemoveIsl,
+    for xx=(ceil(min(g.x))+dxb):dx:(floor(max(g.x))-dxb)
+        ax11=[xx-dx,xx+dx,min(g.y)+dxb,max(g.y)-dxb]
+        N(n)=length(gnew.x);
+        AX(n,:)=ax11;
+        gnew=RemoveMissingIslandsEle(gnew,ax11,p,MinBndDist,0);
+        n=n+1;
+        n/243
+        length(gnew.x)
+        %figure(3);clf;plot(N,'ko-');pause(.001)
+    end
+%    figure(3);clf;plot(AX(:,1),N,'ko-');
+end
+    
+
+g=gnew
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (D) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+g=RemoveSandPointsWW3(g,'RWPS.WW3a1.lakes.msh')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (E) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+p=loadmsh(pslgfile);
+p.x=p.point.coord(:,1);p.x=p.x-360;p.y=p.point.coord(:,2);p.edges=p.edge2.index(:,1:2);
+
+x=g.x;y=g.y;z=g.z;e=g.e;
+if isplot,
+    LS=ComputeLengthScale_wgs84_MEL(x,y,e);LSn=Ele2Nodes(x,y,e,LS);
+    clf;ph=patch(x(e'),y(e'),LSn(e'));cm=colormap('jet');shading interp;axis equal;
+    caxis([0,12])
+    colormap(flip(cm));
+    hold on;
+    plot(p.x,p.y,'k.');
+end
+
+p=loadmsh(pslgfile);
+p.x=p.point.coord(:,1);p.x=p.x-360;p.y=p.point.coord(:,2);p.edges=p.edge2.index(:,1:2);
+
+g0=g;
+gnew=g
+if isplot
+    axAS =[ -179.6738 -159.5513  -18.4086   -7.1731]
+    axis(axAS);pause(5)
+
+    axTofol =[ -197.5575 -196.5479    4.8724    5.7902]
+    axis(axTofol);pause(5)
+
+    axPA = [-177.6646 -175.5927   -0.2906    1.3435]
+    axis(axPA);pause(5)
+
+    axJA =[ -172.1141 -165.7656   14.1706   19.1777]
+    axis(axJA);pause(5)
+
+    axMWI =[ -179.5965 -176.0019   27.0046   29.8397]
+    axis(axMWI);pause(5)
+
+    axTin=[-215.1401 -213.4623   14.3922   15.7155]
+    axis(axTin);pause(5)
+
+    axNNMI =[ -215.9834 -213.8052   19.3525   21.0704];
+    axis(axNNMI);pause(5)
+
+    axNNMI1=[-214.8924 -213.6875   14.6480   15.5983];
+    axis(axNNMI1);pause(5)
+
+    axMaj =[ -177.2629 -175.7651   -0.1340    1.0474]
+    axis(axMaj);pause(5)
+
+    axHI =[ -179.1073 -153.7595   11.4935   31.4855]
+    axis(axHI);pause(5)
+
+    axChuuk =[ -198.1190 -196.0417    4.7967    5.8354]
+    axis(axChuuk);pause(5)
+end
+
+WriteWW3MeshX(gnew,'RWPS.WW3b.lakes.msh')
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (F) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%fix new orleans
+%clear
+g=loadmshWW3('RWPS.WW3b.lakes.msh')
+%Examine and Hand Edit Mesh around new orleans to relect changes if nescesary
+%Should not be nescesary with 
+if isplot,
+    x=g.x;y=g.y;z=g.z;e=g.e;
+    LS=ComputeLengthScale_wgs84_MEL(x,y,e);
+    LSn=Ele2Nodes(x,y,e,LS);
+    clf;ph=patch(x(e'),y(e'),LSn(e'));cm=colormap('jet');shading interp;axis equal;
+    colormap(flip(cm));
+    caxis([0,12]);
+    % Shape file of new marine zones in New Orleans
+    S=shaperead('/scratch3/NCEPDEV/climate/Keston.Smith/RWPS/Data/us_coastline/mz03mr26_LIX.shp')
+    hold on
+    for k=1:length(S),plot(S(k).X,S(k).Y,'k');end
+
+    axNOZ =[  -91.6701  -87.7499   28.7284   30.9173]
+    axis(axNOZ)
+    gnew=g;
+    eno=input('Enter 1 if you want to edit the mesh around New Orleans:' )
+    if eno,
+        gnew=RemoveMeshParts(g,axNOZ,S,[0,50]);
+    else
+        gnew=g;
+    end
+else
+    gnew=g;% no edit
+end
+   
+WriteWW3MeshX(gnew,'RWPS.WW3c.lakes.msh')
+
+%confirm no introduction of sand points
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (G) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+g=RemoveSandPointsWW3(gnew,'RWPS.WW3d.lakes.msh')
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (H) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Fix boundary warping from projection in mesh generation
+%clear
+close all
+g=loadmshWW3('RWPS.WW3d.lakes.msh')
+g0=g;
+
+Blon=[129.91 10.71]
+Blat=[-30.42 79.99]
+Blon(1)=Blon(1)-360
+x=g.x;y=g.y;z=g.z;e=g.e;
+jb=g.bnd;
+bnd0=detbndy(g.e)
+
+if isplot,
+    clf;ph=patch(x(e'),y(e'),z(e'));
+    cm=colormap('jet');shading interp;hold on
+     colormap(flip(cm));
+    axis equal;hold on
+    plot(g.x(jb),g.y(jb),'k.');hold on
+end
+js=find(g.y(jb)<min(Blat));
+g.y(jb(js))=min(Blat);
+if isplot,plot(g.x(jb(js)),g.y(jb(js)),'r.');end
+js=find(g.y(jb)>max(Blat));
+g.y(jb(js))=max(Blat);
+if isplot,plot(g.x(jb(js)),g.y(jb(js)),'r.');end
+
+%dx=degree latitude delta to discriminate land points near boundary from open ocean boundary
+dx=.05;
+js=find(and( g.x(jb)>min(Blon),g.x(jb)<min(Blon)+dx  ));
+g.x(jb(js))=min(Blon);
+if isplot,plot(g.x(jb(js)),g.y(jb(js)),'r.');end
+js=find(and( g.x(jb)<max(Blon),g.x(jb)>max(Blon)-dx  ));
+g.x(jb(js))=max(Blon);
+if isplot,plot(g.x(jb(js)),g.y(jb(js)),'r.');end
+WriteWW3MeshX(g,'RWPS.WW3e.lakes.msh')
+
+%confirm no introduction of sand points
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (I) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+g0=RemoveSandPointsWW3(g,'RWPS.WW3f.lakes.msh')
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% STEP (J) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+WriteWW3MeshX(g0,'RWPS.WW3g.lakes.msh')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Some Plotting
+if isplot,
+    g=g0;
+
+
+    %clear
+    g=loadmshWW3('RWPS.WW3g.lakes.msh')%RWPS.PIXAllLnwps.PP.WW3c.msh');
+    x=g.x;y=g.y;z=g.z;e=g.e;
+    LS=ComputeLengthScale_wgs84_MEL(x,y,e);LSn=Ele2Nodes(x,y,e,LS);
+    clf;ph=patch(x(e'),y(e'),LSn(e'));cm=colormap('jet');shading interp;axis equal;
+    jb=g.bnd;
+    hold on;
+    plot(g.x(jb),g.y(jb),'k.');hold on
+
+
+    f=z;
+    clf;patch(x(e'),y(e'),f(e'));cm=colormap('jet');shading interp;axis equal;
+    colormap(flip(cm));
+    caxis([0,6000]);
+    kprint('Bathy.jpg');
+
+    clf;
+        caxis([0,6000]);
+    colormap(flip(cm));
+
+        hcb=colorbar('h','position',[.1,.075,.8,.025])
+        %hcb.Label.String='(km)'
+        set(gca,'visible','off')
+    kprint('BathyColorbarH.jpg');
+
+    clf;
+    colorbar;
+    colormap(flip(cm));
+    caxis([0,6000]);
+    kprint('BathyColorbarV.jpg');
+
+
+    LS=ComputeLengthScale_wgs84_MEL(x,y,e);
+    LSn=Ele2Nodes(x,y,e,LS);
+
+    clf;patch(x(e'),y(e'),LSn(e'));cm=colormap('jet');shading interp;axis equal;
+    colormap(flip(cm));
+    caxis([0,12]);
+    kprint('Lengthscale.jpg');
+
+
+    axCAR =[  -85.4065  -61.7227    8.8200   26.8661]
+    ax=axCAR
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('Caribean.jpg')
+
+
+    axAK =[ -192.1450 -152.2804   47.2479   77.6231]
+    ax=axAK
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('Alaska.jpg')
+
+    axHI =[ -179.5950 -152.9066   13.8641   34.1996]
+    ax=axHI
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('Hawaii.jpg')
+
+
+    axMI =[ -220.2161 -208.6314   12.5003   21.3274];
+    ax=axMI
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('MarianaIslands.jpg')
+
+    axAS =[ -173.3623 -168.3684  -16.4846  -12.0090]
+    ax=axAS
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('AmericanSamoa.jpg')
+
+
+    axMicro =[ -210.3818 -185.8701   -3.7494   15.5832]
+    ax=axMicro
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('Micronesia.jpg')
+
+    axWMicro =[ -226.6896 -214.3594    5.3753   15.1002]
+    ax=axWMicro
+    axis(ax);daspect([1,cos(pi*ax(3)/180),1])
+    kprint('WestMicronesia.jpg')
+
+
+    figure;
+    clf;
+        caxis([0,12]);
+    colormap(flip(cm));
+
+        hcb=colorbar('h','position',[.1,.075,.8,.025])
+        hcb.Label.String='(km)'
+        set(gca,'visible','off')
+    kprint('LengthScaleColorbarH.jpg');
+
+    clf
+        caxis([0,12]);
+    colormap(flip(cm));
+
+    % hcb=colorbar('v','position',[.1,.075,.8,.025])
+        vcb=colorbar('v','position',[.075,.1,.025,.8])
+        vcb.Label.String='(km)'
+        set(gca,'visible','off')
+    kprint('LengthScaleColorbarV.jpg');
+
+
+    clf
+    caxis([0,6000]);
+    colormap(flip(cm));
+
+        hcb=colorbar('h','position',[.1,.075,.8,.025])
+        hcb.Label.String='(m)'
+        set(gca,'visible','off')
+    kprint('BathyColorbarH.jpg');
+
+    clf
+        caxis([0,6000]);
+    colormap(flip(cm));
+
+    % hcb=colorbar('v','position',[.1,.075,.8,.025])
+        vcb=colorbar('v','position',[.075,.1,.025,.8])
+        vcb.Label.String='(m)'
+        set(gca,'visible','off')
+    kprint('BathyColorbarV.jpg');
+
+end
