cbf43e9e973e99649ca7b7e9360acf8ec39775f1
mingchen-NOAA <ming.chen1@noaa.gov>
2026-03-09 16:54:05 -0400

    Add new feature that can plot global Bias and RMSE (#84)
    * add global bias and RMSE capabilities
    
    * add instructions and examples
    
    * relocate global RMSE color bar to the right, optimize the size of plots, and add statistical results in scatter plots
    
    * add retro plot scripts
    
    * add slurm run jobcards for eval.py and eval_stat.py
    
    * check and copy mvalstats.py and pvalstats.py to current dir
    
    * Use PYTHONPATH to load WW3Tools modules instead of copying files
    
    * fix default WW3-tools name inconsistency issue in job_eval.sh and job_eval_stat.sh

---
+++ ush/retro/job_eval.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=96G
+#SBATCH -q batch
+#SBATCH -t 08:00:00
+#SBATCH -A marine-cpu
+#SBATCH -J eval
+#SBATCH -o logs/eval.out
+
+set -euo pipefail
+
+module use /work2/noaa/marine/jmeixner/general/modulefiles
+module load ww3tools
+
+WORKDIR="/work2/noaa/marine/ming.chen/GFS_Retro_Data"
+WW3TOOLSDIR="${WORKDIR}/WW3-tools/ww3tools"
+
+export PYTHONPATH="${WW3TOOLSDIR}:${PYTHONPATH:-}"
+
+PY_SCRIPT="eval.py"
+
+python -u "${PY_SCRIPT}"
